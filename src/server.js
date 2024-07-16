548330620b0452ac0d24997d7ff81595a3dcd733
Oleksandr-Zibnytskyi <xander.en.87@gmail.com>
2024-07-16 09:55:58 +0200

    upd

--- src/server.js
+++ src/server.js
@@ -2,20 +2,29 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
-import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
+import { env } from './utils/env.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export async function setupServer() {
   const app = express();
+  const PORT = Number(env('PORT', '3000'));
 
   app.use(cors());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
 
-
   app.get('/contacts', async (req, res, next) => {
     try {
       const contacts = await getAllContacts();
-      res.status(200).json({ status: 200, message: 'Successfully found contacts', data: contacts });
+      res
+        .status(200)
+        .json({
+          status: 200,
+          message: 'Successfully found contacts',
+          data: contacts,
+        });
     } catch (error) {
       next(error);
     }
@@ -41,24 +50,16 @@ export async function setupServer() {
     }
   });
 
-
   app.use((req, res) => {
     res.status(404).send({ message: 'Not found' });
   });
-
 
   app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
   });
 
-
-  const PORT = Number(env('PORT', '3000'));
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
-
-
-
-
