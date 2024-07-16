b7df2ff067336db966a0777c5a108e34a86a2bc3
Oleksandr-Zibnytskyi <xander.en.87@gmail.com>
2024-07-16 12:01:16 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 export async function setupServer() {
   const app = express();
-  const PORT = Number(env('PORT', '3000'));
+  const PORT = Number(env('PORT', '4000'));
 
   app.use(cors());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
