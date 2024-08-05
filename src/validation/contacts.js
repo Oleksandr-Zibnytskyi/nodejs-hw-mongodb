8831343f0706480293af7fc55ac671bac5e5e13b
Oleksandr-Zibnytskyi <xander.en.87@gmail.com>
2024-08-05 10:04:49 +0200

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,17 +1,17 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+  name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
   isFavourite: Joi.boolean()
 });
 
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30),
-  phoneNumber: Joi.string().min(3).max(20).pattern(/^[0-9+]{6,16}$/),
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
   isFavourite: Joi.boolean()
